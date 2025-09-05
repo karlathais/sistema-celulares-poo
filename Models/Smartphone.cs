a89086867f1b7d6709f7048dd0349501439a822c
karlathais <karllathaysk@gmail.com>
2025-09-04 22:21:48 -0300

    sistema-celulares-poo

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,25 +1,24 @@
+using System;
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; }
+        public string Imei { get; set; }
+        public int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
 
-        public void Ligar()
-        {
-            Console.WriteLine("Ligando...");
-        }
-
-        public void ReceberLigacao()
-        {
-            Console.WriteLine("Recebendo ligação...");
-        }
+        public void Ligar() => Console.WriteLine($"{Modelo} está ligando...");
+        public void ReceberLigacao() => Console.WriteLine($"{Modelo} está recebendo ligação...");
 
         public abstract void InstalarAplicativo(string nomeApp);
     }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,33 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Smartphone iphone = new Iphone("9891-9000", "iPhone 14", "IMEI98765", 128);
+            Smartphone nokia = new Nokia("1234-5678", "Nokia 3310", "IMEI12345", 64);
+
+            TestarSmartphone(iphone, "Instagram");
+            Console.WriteLine();
+            TestarSmartphone(nokia, "WhatsApp");
+
+            Console.WriteLine("\nTestes finalizados!");
+        }
+
+        static void TestarSmartphone(Smartphone celular, string app)
+        {
+            Console.WriteLine($"=== Testando {celular.Modelo} ===");
+            Console.WriteLine($"Número: {celular.Numero}");
+            Console.WriteLine($"Modelo: {celular.Modelo}");
+            Console.WriteLine($"IMEI: {celular.Imei}");
+            Console.WriteLine($"Memória: {celular.Memoria}GB");
+
+            celular.Ligar();
+            celular.ReceberLigacao();
+            celular.InstalarAplicativo(app);
+        }
+    }
+}
